b07c127d178d7922dcc42c02c0bd130a7dcbdbf9
murataka-free <murataka1128@outlook.jp>
2020-08-16 10:45:47 +0900

    地図表示修正

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var options = {
     timeout: 5000,
     maximumAge: 0
   };
+
   
   function success(pos) {
     var crd = pos.coords;
@@ -12,9 +13,22 @@ var options = {
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
+// 地図を作成する
+var mymap = L.map('map');
+ 
+// タイルレイヤーを作成し、地図にセットする
+L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+}).addTo(mymap);
+ 
+// 地図の中心座標とズームレベルを設定する
+mymap.setView([crd.latitude, crd.longitude], 13);
 
-    alert(`Latitude : ${crd.latitude}`);
-    alert(`Longitude: ${crd.longitude}`);
+var marker = L.marker([crd.latitude, crd.longitude]).addTo(mymap);
+ 
+// クリックした際にポップアップメッセージを表示する
+marker.bindPopup("あなたの場所です");
 
     
   }
